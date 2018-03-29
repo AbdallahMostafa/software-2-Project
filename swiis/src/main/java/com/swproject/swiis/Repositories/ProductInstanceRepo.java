76d5592f2f4547261a8d656f29ef12d5e116b9f5
Mostafa Garana <mostafagarana@gmail.com>
2018-03-29 06:33:17 +0200

    Add files via upload

---
+++ swiis/src/main/java/com/swproject/swiis/Repositories/ProductInstanceRepo.java
@@ -0,0 +1,9 @@
+package com.swproject.swiis.Repositories;
+
+import com.swproject.swiis.Entity.ProductInstance;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductInstanceRepo extends CrudRepository<ProductInstance,Long> {
+
+}
+
